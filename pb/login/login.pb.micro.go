2f05c0cebb4b1bb4ca598d7530adb2d42464867f
saying-yan <1396318209@qq.com>
2022-10-31 16:46:52 +0800

    gen pb file

--- pb/login/login.pb.micro.go
+++ pb/login/login.pb.micro.go
@@ -5,14 +5,15 @@ package login
 
 import (
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
+	proto "google.golang.org/protobuf/proto"
 	math "math"
 )
 
 import (
 	context "context"
-	client "github.com/micro/go-micro/client"
-	server "github.com/micro/go-micro/server"
+	api "go-micro.dev/v4/api"
+	client "go-micro.dev/v4/client"
+	server "go-micro.dev/v4/server"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -20,16 +21,17 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the proto package it is being compiled against.
-// A compilation error at this line likely means your copy of the
-// proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
-
 // Reference imports to suppress errors if they are not otherwise used.
+var _ api.Endpoint
 var _ context.Context
 var _ client.Option
 var _ server.Option
+
+// Api Endpoints for Login service
+
+func NewLoginEndpoints() []*api.Endpoint {
+	return []*api.Endpoint{}
+}
 
 // Client API for Login service
 
@@ -46,12 +48,6 @@ type loginService struct {
 }
 
 func NewLoginService(name string, c client.Client) LoginService {
-	if c == nil {
-		c = client.NewClient()
-	}
-	if len(name) == 0 {
-		name = "login"
-	}
 	return &loginService{
 		c:    c,
 		name: name,
