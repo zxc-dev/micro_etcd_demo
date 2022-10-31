3852d2698ad9109e065c1e50c6ea13ccdf19402b
zxc <2218751654@qq.com>
2022-10-31 17:44:12 +0800

    zxc

--- login_service/model/user.go
+++ login_service/model/user.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func InitMysql() {
 	var err error
-	DB, err = gorm.Open("mysql", "root:micro.demo.password@(127.0.0.1:13306)/micro_demo?charset=utf8&parseTime=True&loc=Local")
+	DB, err = gorm.Open("mysql", "root:micro.demo.password@(127.0.0.1:3306)/micro_demo?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Errorf("创建数据库连接失败:%v", err)
 	}
